af6e1478062cad77f8ba25c09c9d7fd1f9dedf4a
walid <walid.gammoudi@esprit.tn>
2023-06-07 16:37:06 +0200

    first commit

---
+++ pfeefe/src/main/java/com/example/pfeefe/PfeefeApplication.java
@@ -0,0 +1,14 @@
+package com.example.pfeefe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfeefeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfeefeApplication.class, args);
+		
+	}
+
+}
---
+++ pfeefe/src/test/java/com/example/pfeefe/PfeefeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pfeefe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfeefeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
